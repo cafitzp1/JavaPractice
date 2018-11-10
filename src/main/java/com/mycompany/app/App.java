f21c9e1f63869eefdd2622c2ef2c2b27416b78ec
Connor <cafitzp1@asu.edu>
2018-11-09 21:05:27 -0700

    ...

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -8,13 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println(shapeArea(2));
-    }
-
-    private static int shapeArea(int n) {
-
-        if (n==1) return 1;
-
-        return (shapeArea(n-1)) + (n-1)*4;
+        System.out.println("Hello.");
     }
 }
