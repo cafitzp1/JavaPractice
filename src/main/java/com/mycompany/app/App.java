dbedf913d9a14e9ccff17a441ba629ff9cc7b25c
Connor <cafitzp1@asu.edu>
2018-11-09 23:43:27 -0700

    ...

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -1,13 +1,9 @@
 package com.mycompany.app;
 
-// 1, 2, 3, 4
-// 1, 4, 8, 12
-// 1, 5, 13, 25
-
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println("Hello.");
+        System.out.println("Hello");
     }
 }
