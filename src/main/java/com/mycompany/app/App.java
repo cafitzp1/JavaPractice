ba23c182e78a15c92014d06997e207ef621c7fa3
Connor <cafitzp1@asu.edu>
2018-11-10 00:11:42 -0700

    ...

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -1,9 +1,34 @@
 package com.mycompany.app;
 
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println("Hello");
+public class App {
+    public static void main(String[] args) {
+        String str = "We have a large inventory of things in our warehouse falling in "
+                + "the title category:apparell and the slightly "
+                + "more in demand category:makeup along with the category:furniture and _.";
+
+        printCategories(str);
+    }
+
+    static private void printCategories(String str) {
+
+        if (str.length() <= 0 || str.indexOf("category") == -1)
+            return;
+
+        String newStr = "";
+        int startIndex = str.indexOf("category")+1,
+            activeIndex = startIndex,
+            endIndex = startIndex;
+            
+        startIndex += "category".length();
+
+        while (str.charAt(activeIndex++) != ' ') {
+            endIndex++;
+            if(activeIndex == str.length()-1) return;
+        }
+
+        newStr = str.substring(startIndex, endIndex);
+        System.out.println(newStr);
+
+        printCategories(str.substring(endIndex, str.length() - 1));
     }
 }
