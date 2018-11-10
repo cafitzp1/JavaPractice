7b33856e67e6a037719978ad86d890c49ff0017d
Connor <cafitzp1@asu.edu>
2018-11-10 15:37:39 -0700

    ...

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -4,6 +4,28 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println("Hello");
+        int[] intArray = new int[] {20, 35, -15, 7, 55, 1, -22 };
+
+        for (int lastUnsortedIndex = intArray.length-1; lastUnsortedIndex > 0; lastUnsortedIndex--) {
+            for (int i = 0; i < lastUnsortedIndex; i++) {
+                if (intArray[i] > intArray[i + 1]) {
+                    swap(intArray, i, i + 1);
+                }
+            }
+        }
+
+        for (int i = 0; i<intArray.length; i++) {
+            System.out.println(intArray[i]);
+        }
+    }
+
+    private static void swap(int[] arr, int i, int j) {
+        // nothing to swap, two elements are the same
+        if (i == j) return;
+
+        // swap the elements
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
     }
 }
