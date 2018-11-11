8d672c8598b7c452559e332194bb07680d6455e1
Connor <cafitzp1@asu.edu>
2018-11-10 20:37:09 -0700

    ...

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -4,6 +4,55 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println("Hello");
+        int[] intArray = { 20, 35, -15, 7, 55, 1, -22 };
+
+        mergeSort(intArray, 0, intArray.length);
+
+        for (int i = 0; i < intArray.length; i++) {
+            System.out.println(intArray[i]);
+        }
     }
-}
+
+    private static void mergeSort(int[] input, int start, int end) {
+
+        if (end - start < 2)
+            return;
+
+        int mid = (start + end) / 2;
+        
+        // handle the left side sorting
+        mergeSort(input, start, mid);
+
+        // handle the right side sorting
+        mergeSort(input, mid, end);
+
+        // handle the left and right
+        merge(input, start, mid, end);
+    }
+
+    private static void merge(int[] input, int start, int mid, int end) {
+
+        // both are sorted, so return if the last element of the left partition
+        // is less than or equal to the first element of the right partition
+        if (input[mid - 1] <= input[mid])
+            return;
+
+        int i = start,
+            j = mid,
+            tempIndex = 0;
+
+        int[] temp = new int[end-start];
+
+        while (i < mid && j < end) {
+            temp[tempIndex++] = input[i] <= input[j] ?
+                input[i++] :
+                input[j++];
+        }
+
+        System.arraycopy(input, i, input, start + tempIndex, mid - i);
+        System.arraycopy(temp, 0, input, start, tempIndex);
+
+        }
+    }
+    
+    
