c71e4898f0765a5ae0b3c52c2ddf3c5d299ca49c
Connor <cafitzp1@asu.edu>
2018-11-11 17:51:11 -0700

    first solution

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -6,26 +6,31 @@ public class App
     {
         int[] intArray = { 20, 35, -15, 7, 55, 1, -22 };
 
-        // element in first index is in the sorted partition
-        for (int firstUnsortedIndex = 1; firstUnsortedIndex < intArray.length; firstUnsortedIndex++) {
-
-            // save value of element we are going to insert so it doesn't get overwritten
-            int newElement = intArray[firstUnsortedIndex];
-            int i;
-
-            // traverse sorted partition and look for correct position to insert element
-            // keep looking until we hit the front of the array and or we find an element
-            // less than or equal to the element we are trying to insert
-            for (i = firstUnsortedIndex; i > 0 && intArray[i - 1] > newElement; i--) {
-                intArray[i] = intArray[i - 1];
-            }
-
-            // when we drop out of the loop, we want to insert our element
-            intArray[i] = newElement;
-        }
+        recursiveInsertionSort(intArray, 0, intArray.length);
 
         for (int i = 0; i < intArray.length; i++) {
             System.out.println(intArray[i]);
         }
+    }
+
+    private static void recursiveInsertionSort(int[] intArray, int start, int end) {
+
+        // return if start crosses end
+        if(start + 1 >= end)
+            return;
+
+        // we do not have to check the first value as this is in the sorted 
+        // partition already
+        int firstUnsortedIndex = start + 1; 
+        int newElement = intArray[firstUnsortedIndex];
+        int i;
+
+        for (i = firstUnsortedIndex; i > 0 && intArray[i - 1] > newElement; i--) {
+            intArray[i] = intArray[i - 1];
+        }
+
+        intArray[i] = newElement;
+
+        recursiveInsertionSort(intArray, firstUnsortedIndex, end);
     }
 }
