aea0200359d743a153c891eca2b471563580eba4
Connor <cafitzp1@asu.edu>
2018-11-11 17:32:39 -0700

    added comments

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -6,15 +6,21 @@ public class App
     {
         int[] intArray = { 20, 35, -15, 7, 55, 1, -22 };
 
+        // element in first index is in the sorted partition
         for (int firstUnsortedIndex = 1; firstUnsortedIndex < intArray.length; firstUnsortedIndex++) {
-            int newElement = intArray[firstUnsortedIndex];
 
+            // save value of element we are going to insert so it doesn't get overwritten
+            int newElement = intArray[firstUnsortedIndex];
             int i;
 
+            // traverse sorted partition and look for correct position to insert element
+            // keep looking until we hit the front of the array and or we find an element
+            // less than or equal to the element we are trying to insert
             for (i = firstUnsortedIndex; i > 0 && intArray[i - 1] > newElement; i--) {
                 intArray[i] = intArray[i - 1];
             }
 
+            // when we drop out of the loop, we want to insert our element
             intArray[i] = newElement;
         }
 
