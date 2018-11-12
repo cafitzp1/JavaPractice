d76e9dc31c494e6f2097d78b8bcb6b37e111a8a5
Connor <cafitzp1@asu.edu>
2018-11-12 00:15:01 -0700

    fixed solution (didn't account for negative numbers)

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -3,13 +3,14 @@ package com.mycompany.app;
 public class App {
     public static void main(String[] args) {
 
-        System.out.println(reverseNumber(523));
+        System.out.println(reverseNumber(-123));
     }
 
     private static int reverseNumber(int num) {
 
         int revNum = 0;
-        while (num > 0) {
+
+        while (num != 0) {
             revNum = revNum * 10 + num % 10;
             num /= 10;
         }
