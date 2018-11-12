02a6411eca3f1f0a358ee2df3e9998ccebd28ab0
Connor <cafitzp1@asu.edu>
2018-11-11 23:52:32 -0700

    shorter solution

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -16,13 +16,7 @@ public class App {
     }
 
     public static int[] twoSum(int[] nums, int target) {
-        if (nums == null || nums.length < 2)
-            return new int[] { 0, 0 };
-
-        if (nums.length == 2)
-            return new int[] { 0, 1 };
-
-        HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
+        HashMap<Integer, Integer> map = new HashMap<Integer,Integer>();
         for (int i = 0; i < nums.length; i++) {
             int complement = target - nums[i];
             if (map.containsKey(complement)) {
@@ -30,7 +24,6 @@ public class App {
             }
             map.put(nums[i], i);
         }
-
-        return new int[] { 0, 0 };
+        throw new IllegalArgumentException();
     }
 }
