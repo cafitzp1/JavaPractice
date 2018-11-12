ccff9483f3edb2efdc1749790dae8d3e2c50d5f5
Connor <cafitzp1@asu.edu>
2018-11-11 18:54:57 -0700

    solution from udemy

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -2,56 +2,57 @@ package com.mycompany.app;
 
 public class App {
     public static void main(String[] args) {
-        int[] radixArray = { 4725, 4586, 1330, 8792, 1594, 5729 };
+        String[] radixArray = { "bcdef", "dbaqc", "abcde", "omadd", "bbbbb" };
 
-        radixSort(radixArray, 10, 4);
+        radixSort(radixArray, 26, 5);
 
         for (int i = 0; i < radixArray.length; i++) {
             System.out.println(radixArray[i]);
         }
     }
 
-    private static void radixSort(int[] input, int radix, int width) {
-        for (int i = 0; i < width; i++) {
-            // call for each position in our values (the radix, so 4 loops)
+    private static void radixSort(String[] input, int radix, int width) {
+        // NOTE: we want to start from the right of the input value
+        // and go to the left. Our method for getting the decimal had
+        // us getting the index differently so we had our loop indexer
+        // incrementing. For letters, we can just use the width-1 to get
+        // the char for the specified position
+        for (int i = width-1; i >= 0; i--) {
             radixSingleSort(input, i, radix);
         }
     }
 
-    private static void radixSingleSort(int[] input, int position, int radix) {
+    private static void radixSingleSort(String[] input, int position, int radix) {
 
-        // store amount of items we are sorting
         int numItems = input.length;
 
-        // create temp array to hold our count values
+        // array of length 26 for counting letters
         int[] countArray = new int[radix];
 
-        // count number of values that have a specific digit in the 
-        // specified position
-        for (int value: input) {
-            countArray[getDigit(position, value, radix)]++;
+        // count the letters
+        for (String value: input) {
+            countArray[getLetterIndex(position, value)]++;
         }
-        // adjust count array to countain number of values with that
-        // digit or less than that digit for this position
+        // adjust count
         for (int j = 1; j < radix; j++) {
             countArray[j] += countArray[j - 1];
         }
-        // copy values into temporary array, right to left to preserver
-        // duplicate values (must be a stable sort)
-        int[] temp = new int[numItems];
+        // copy values into temp array
+        String[] temp = new String[numItems];
         for (int tempIndex = numItems-1; tempIndex >= 0; tempIndex--) {
-            temp[--countArray[getDigit(position, input[tempIndex], radix)]] =
+            temp[--countArray[getLetterIndex(position, input[tempIndex])]] =
                 input[tempIndex];
         }
-        // copy back from temporary array into input array
+        // copy back from temp array into input array
         for (int tempIndex = 0; tempIndex < numItems; tempIndex++) {
             input[tempIndex] = temp[tempIndex];
         }
     }
 
-    private static int getDigit(int position, int value, int radix) {
-        // divides the number by 10^position, modded by the radix
-        // in the end, we get the remainder which will be the digit
-        return value / (int)Math.pow(radix, position) % radix;
+    private static int getLetterIndex(int position, String str) {
+        // char is just a numeric value behind the scenes
+        // a is 97 (A is 65) in ASCII, so whatever letter
+        // we need we can get by subtracting 'a'
+        return str.charAt(position) - 'a';
     }
 }
