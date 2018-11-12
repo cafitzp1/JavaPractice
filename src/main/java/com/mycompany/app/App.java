aed432ab1aaa95efd4e2eb52c8f510fbdafe6d9d
Connor <cafitzp1@asu.edu>
2018-11-12 00:23:07 -0700

    solution to check for int overflow

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -8,13 +8,16 @@ public class App {
 
     private static int reverseNumber(int num) {
 
-        int revNum = 0;
-
+        int rev = 0;
         while (num != 0) {
-            revNum = revNum * 10 + num % 10;
+            int pop = num % 10;
             num /= 10;
+            if (rev > Integer.MAX_VALUE / 10 || (rev == Integer.MAX_VALUE / 10 && pop > 7))
+                return 0;
+            if (rev < Integer.MIN_VALUE / 10 || (rev == Integer.MIN_VALUE / 10 && pop < -8))
+                return 0;
+            rev = rev * 10 + pop;
         }
-
-        return revNum;
+        return rev;
     }
 }
