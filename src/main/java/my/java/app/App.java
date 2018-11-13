6e06efb5d73bc42255b32f6e2538f34791e6c301
Connor <cafitzp1@asu.edu>
2018-11-13 00:36:42 -0700

    my solution

--- src/main/java/my/java/app/App.java
+++ src/main/java/my/java/app/App.java
@@ -1,9 +1,34 @@
 package my.java.app;
 
+import java.util.HashSet;
+import java.lang.Character;
+
 public class App {
     public static void main(String[] args) {
 
-        System.out.println("Hi there");
+        System.out.println(lengthOfLongestSubstring("abvkdkreob"));
 
+    }
+
+    private static int lengthOfLongestSubstring(String s) {
+        HashSet<Character> set;
+        int longest = 0;
+
+        for (int i = 0, j = 0; i < s.length(); i++) {
+            j = i;
+            set = new HashSet<>();
+            char c;
+            while (j < s.length()) {
+                if (!set.contains(((c = s.charAt(j))))) {
+                    set.add(c);
+                    j++;
+                } else {
+                    break;
+                }
+            }
+            if (j-i > longest) longest = j-i;
+        }
+
+        return longest;
     }
 }
