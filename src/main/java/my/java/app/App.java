a00b0b8ae8494ebbfe43a0a77f4891ad6b151e50
Connor <cafitzp1@asu.edu>
2018-11-14 00:52:26 -0700

    added JDK LinkedList usage as stack and queue without wrapper classes

--- src/main/java/my/java/app/App.java
+++ src/main/java/my/java/app/App.java
@@ -1,11 +1,40 @@
 package my.java.app;
 
+import java.util.LinkedList;
+
 public class App {
 
     public static void main(String[] args) {
         
-        //ArrayStack stack = new ArrayStack(10);
+    }
 
+    public static boolean checkForPalindrome(String string) {
+
+        // using JDK LinkedList without any wrapper class
+        LinkedList<Character> stack = new LinkedList<Character>();
+        LinkedList<Character> queue = new LinkedList<Character>();
+        String lowerCase = string.toLowerCase();
+
+        for (int i = 0; i < lowerCase.length(); i++) {
+            char c = lowerCase.charAt(i);
+            if (c >= 'a' && c <= 'z') {
+                queue.addLast(c);
+                stack.push(c);
+            }
+        }
+
+        while(!stack.isEmpty()) {
+            if (!stack.pop().equals(queue.removeFirst())) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public static void Test() {
+
+        // for testing our own wrapper stack and queue classes
         Employee jane = new Employee("Jane", "Jones", 123);
         Employee john = new Employee("John", "Doe", 4567);
         Employee mary = new Employee("Mary", "Smith", 22);
