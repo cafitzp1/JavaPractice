55424e36efad43381b9e38dd4e5f4ddbc88e044a
Connor <cafitzp1@asu.edu>
2018-11-13 19:40:32 -0700

    array queue implementation

--- src/main/java/my/java/app/App.java
+++ src/main/java/my/java/app/App.java
@@ -3,18 +3,38 @@ package my.java.app;
 public class App {
 
     public static void main(String[] args) {
-        
-        ArrayStack stack = new ArrayStack(10);
 
-        stack.push(new Employee("Jane", "Jones", 123));
-        stack.push(new Employee("John", "Doe", 4567));
-        stack.push(new Employee("Mary", "Smith", 22));
-        stack.push(new Employee("Mike", "Wilson", 3245));
-        stack.push(new Employee("Bill", "End", 78));
+        Employee jane = new Employee("Jane", "Jones", 123);
+        Employee john = new Employee("John", "Doe", 4567);
+        Employee mary = new Employee("Mary", "Smith", 22);
+        Employee mike = new Employee("Mike", "Wilson", 3245);
+        Employee bill = new Employee("Bill", "End", 78);
+
+        ArrayQueue queue = new ArrayQueue(10);
+        queue.add(jane);
+        queue.add(john);
+        queue.add(mary);
+        queue.add(mike);
+        queue.add(bill);
+        // queue.printQueue();
+
+        queue.remove();
+        queue.remove();
+        // queue.printQueue();
+
+        // System.out.println(queue.peek());
+        queue.remove();
+        queue.remove();
+        queue.remove();
+        // queue.remove();
+
+        queue.add(mike);
+
+        queue.printQueue();
 
         // stack.printStack();
         // System.out.println(stack.peek());
-        System.out.println("Popped: " + stack.pop());
-        System.out.println(stack.peek());
+        // System.out.println("Popped: " + stack.pop());
+        // System.out.println(stack.peek());
     }
 }
