ab11c435a9d3a2579651e9cc61dee413005af4ec
Connor <cafitzp1@asu.edu>
2018-11-13 18:01:04 -0700

    ...

--- src/main/java/my/java/app/App.java
+++ src/main/java/my/java/app/App.java
@@ -4,17 +4,24 @@ public class App {
 
     public static void main(String[] args) {
         
-        ArrayStack stack = new ArrayStack(10);
+        //ArrayStack stack = new ArrayStack(10);
 
-        stack.push(new Employee("Jane", "Jones", 123));
-        stack.push(new Employee("John", "Doe", 4567));
-        stack.push(new Employee("Mary", "Smith", 22));
-        stack.push(new Employee("Mike", "Wilson", 3245));
-        stack.push(new Employee("Bill", "End", 78));
+        Employee jane = new Employee("Jane", "Jones", 123);
+        Employee john = new Employee("John", "Doe", 4567);
+        Employee mary = new Employee("Mary", "Smith", 22);
+        Employee mike = new Employee("Mike", "Wilson", 3245);
+        Employee bill = new Employee("Bill", "End", 78);
 
-        // stack.printStack();
-        // System.out.println(stack.peek());
+        LinkedStack stack = new LinkedStack();
+        stack.push(jane);
+        stack.push(john);
+        stack.push(mary);
+        stack.push(mike);
+        stack.push(bill);
+
+        //System.out.println(stack.peek());
         System.out.println("Popped: " + stack.pop());
-        System.out.println(stack.peek());
+        stack.printStack();
+        // System.out.println(stack.peek());
     }
 }
