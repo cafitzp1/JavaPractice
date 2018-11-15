4e5e17490c5fec689c16ad3fb1a6b12d8ec83d26
Connor <cafitzp1@asu.edu>
2018-11-14 18:31:29 -0700

    hashmap implementation using JDK HashMap class

--- src/main/java/my/java/app/App.java
+++ src/main/java/my/java/app/App.java
@@ -1,5 +1,9 @@
 package my.java.app;
 
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+
 public class App {
     public static void main(String[] args) {
 
@@ -7,22 +11,37 @@ public class App {
         Employee john = new Employee("John", "Doe", 4567);
         Employee mary = new Employee("Mary", "Smith", 22);
         Employee mike = new Employee("Mike", "Wilson", 3245);
-        // Employee bill = new Employee("Bill", "End", 78);
 
-        ChainedHashtable ht = new ChainedHashtable();
-        ht.put("Jones", jane);
-        ht.put("Doe", john);
-        ht.put("Wilson", mike);
-        ht.put("Smith", mary);
+        Map<String, Employee> hashMap = new HashMap<>();
 
-        // ht.printHashtable();
+        // put into map; will overwrite existing keys' values
+        hashMap.put("Jones", jane);
+        hashMap.put("Doe", john);
+        hashMap.put("Smith", mary);
+        hashMap.put("Doe", mary);
 
-        System.out.println("Retrieve key Smith: " + ht.get("Smith"));
-        
-        ht.remove("Doe");
-        ht.remove("Jones");
-        ht.printHashtable();
-        
-        System.out.println("Retrieve key Smith: " + ht.get("Smith"));
+        // put if absent (returns exisiting val if not absent)
+        System.out.println(hashMap.putIfAbsent("Doe", mike));
+
+        // remove employees; returns removed employee
+        System.out.println(hashMap.remove("Jones"));
+
+        // get an employee
+        System.out.println(hashMap.get("Smith"));
+        System.out.println(hashMap.get("Smithxxx"));
+        System.out.println(hashMap.getOrDefault("Smithxxx", mike));
+
+        // see if map contains a key or value
+        System.out.println(hashMap.containsKey("Doe"));
+        System.out.println(hashMap.containsValue(jane));
+
+        // loop the map
+        Iterator<Employee> itr = hashMap.values().iterator();
+        while (itr.hasNext()) {
+            System.out.println(itr.next());
+        }
+
+        // alternative way to loop
+        hashMap.forEach((k, v) -> System.out.println("Key = " + k + ", Employee = " + v));
     }
 }
