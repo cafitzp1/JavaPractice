fc0eaa98a8c043abdb0057b695e7c7d37b1ec866
Connor <cafitzp1@asu.edu>
2018-11-15 14:54:23 -0700

    searchAlgorithms

--- src/main/java/my/java/app/App.java
+++ src/main/java/my/java/app/App.java
@@ -6,4 +6,12 @@ public class App {
         System.out.println("Hi there");
 
     }
+
+    private static int linearSearch(int[] input, int value) {
+        for (int i = 0; i < input.length; i++) {
+            if (input[i] == value)
+                return i;
+        }
+        return -1;
+    }
 }
