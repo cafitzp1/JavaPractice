f4cc1112afbd492c484b37af56878431e4ab2ba7
Connor <cafitzp1@asu.edu>
2018-11-15 12:50:16 -0700

    bucket sort implementation with insertion and merge sort

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -1,31 +1,121 @@
 package com.mycompany.app;
 
-public class App 
-{
-    public static void main( String[] args )
-    {
-        int[] intArray = { 20, 35, -15, 7, 55, 1, -22 };
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
 
-        // element in first index is in the sorted partition
-        for (int firstUnsortedIndex = 1; firstUnsortedIndex < intArray.length; firstUnsortedIndex++) {
+public class App {
+    public static void main(String[] args) {
+        // int[] intArray = { 20, 35, -15, 7, 55, 1, -22 };
+        int[] intArray = { 54, 46, 83, 66, 95, 92, 43 };
 
-            // save value of element we are going to insert so it doesn't get overwritten
-            int newElement = intArray[firstUnsortedIndex];
-            int i;
-
-            // traverse sorted partition and look for correct position to insert element
-            // keep looking until we hit the front of the array and or we find an element
-            // less than or equal to the element we are trying to insert
-            for (i = firstUnsortedIndex; i > 0 && intArray[i - 1] > newElement; i--) {
-                intArray[i] = intArray[i - 1];
-            }
-
-            // when we drop out of the loop, we want to insert our element
-            intArray[i] = newElement;
-        }
+        bucketSort(intArray);
+        // insertionSort(intArray);
 
         for (int i = 0; i < intArray.length; i++) {
             System.out.println(intArray[i]);
         }
     }
+
+    public static void bucketSort(int[] input) {
+        List<Integer>[] buckets = new List[10];
+
+        // create array list at each bucket
+        for (int i = 0; i < buckets.length; i++) {
+            buckets[i] = new ArrayList<Integer>();
+        }
+
+        // scatter values, hash, put into appropriate bucket
+        for (int i = 0; i < input.length; i++) {
+            buckets[hash(input[i])].add(input[i]);
+        }
+
+        // sort each bucket
+        System.out.println("Length: " + buckets.length);
+        for (List<Integer> bucket : buckets) {
+            // using JDK sort
+            // Collections.sort(bucket);
+
+            // using my generic insertion sort
+            // insertionSort(bucket);
+
+            // using my generic merge sort
+            mergeSort(bucket, 0, bucket.size());
+        }
+
+        // traverse and copy sorted values from each list
+        int j = 0;
+        // traverse buckets array
+        for (int i = 0; i < buckets.length; i++) {
+            // traverse array list at buckets[i]
+            for (int value : buckets[i]) {
+                // copy into input array
+                input[j++] = value;
+            }
+        }
+    }
+
+    private static int hash(int value) {
+        // ex: (54 / 10 = 5) % 10 = 5
+        return value / (int) 10 % 10;
+    }
+
+    public static <T extends Comparable<T>> void insertionSort(List<T> list) {
+
+        for (int firstUnsortedIndex = 1; firstUnsortedIndex < list.size(); firstUnsortedIndex++) {
+
+            T newElement = list.get(firstUnsortedIndex);
+            int i;
+            for (i = firstUnsortedIndex; i > 0 && list.get(i - 1).compareTo(newElement) == 1; i--) {
+                list.set(i, list.get(i - 1));
+            }
+
+            list.set(i, newElement);
+        }
+    }
+
+    public static void mergeSort(List<Integer> list, int start, int end) {
+        if (end - start < 2)
+            return;
+
+        int mid = (start + end) / 2;
+
+        mergeSort(list, start, mid);
+        mergeSort(list, mid, end);
+        merge(list, start, mid, end);
+    }
+
+    public static void merge(List<Integer> list, int start, int mid, int end) {
+
+        Integer[] input = list.toArray(new Integer[list.size()]);
+
+        if (list.get(mid - 1).compareTo(list.get(mid)) < 1)
+            return;
+
+        int i = start, j = mid, index = 0;
+
+        Integer[] temp = new Integer[end - start];
+
+        while (i < mid && j < end) {
+            temp[index++] = list.get(i).compareTo(list.get(j)) < 1 ? list.get(i++) : list.get(j++);
+        }
+
+        System.arraycopy(input, i, input, start + index, mid - i);
+        System.arraycopy(temp, 0, input, start, index);
+
+        list.clear();
+        list.addAll(Arrays.asList(input));
+    }
+
+    // arraylist 'list' to array 'input'
+    // Integer[] input = list.toArray(new Integer[list.size()]);
+
+    // array 'input' to arraylist 'list'
+    // list = new ArrayList<Integer>(Arrays.asList(input));
+
+    // generic array instantiation
+    // @SuppressWarnings("unchecked")
+    // T[] newArray = (T[]) new Object[newSize];
 }
