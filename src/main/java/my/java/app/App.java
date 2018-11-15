4d3ead6b285cd5fc0765d092db2bca11f1ac8d75
Connor <cafitzp1@asu.edu>
2018-11-14 17:50:15 -0700

    hashtable chaining implementation

--- src/main/java/my/java/app/App.java
+++ src/main/java/my/java/app/App.java
@@ -7,20 +7,19 @@ public class App {
         Employee john = new Employee("John", "Doe", 4567);
         Employee mary = new Employee("Mary", "Smith", 22);
         Employee mike = new Employee("Mike", "Wilson", 3245);
-        Employee bill = new Employee("Bill", "End", 78);
+        // Employee bill = new Employee("Bill", "End", 78);
 
-        SimpleHashtable ht = new SimpleHashtable();
+        ChainedHashtable ht = new ChainedHashtable();
         ht.put("Jones", jane);
         ht.put("Doe", john);
         ht.put("Wilson", mike);
         ht.put("Smith", mary);
 
-        ht.printHashtable();
+        // ht.printHashtable();
 
-        System.out.println("Retrieve key Wilson: " + ht.get("Wilson"));
         System.out.println("Retrieve key Smith: " + ht.get("Smith"));
         
-        ht.remove("Wilson");
+        ht.remove("Doe");
         ht.remove("Jones");
         ht.printHashtable();
         
