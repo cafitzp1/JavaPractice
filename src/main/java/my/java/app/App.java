85e58f8cb23be0ab8234577783a54c4300a0efb3
Connor <cafitzp1@asu.edu>
2018-11-16 19:29:13 -0700

    added delete, fixHeapBelow, and peek methods as well as tests in main method

--- src/main/java/my/java/app/App.java
+++ src/main/java/my/java/app/App.java
@@ -3,7 +3,23 @@ package my.java.app;
 public class App {
     public static void main(String[] args) {
 
-        System.out.println("Hi there");
+        Heap heap = new Heap(10);
+
+        heap.insert(80);
+        heap.insert(75);
+        heap.insert(60);
+        heap.insert(68);
+        heap.insert(55);
+        heap.insert(40);
+        heap.insert(52);
+        heap.insert(67);
+
+        heap.printHeap();
+        
+        heap.delete(0);
+        heap.printHeap();
+
+        System.out.println(heap.peek());
 
     }
 }
--- src/main/java/my/java/app/Heap.java
+++ src/main/java/my/java/app/Heap.java
@@ -20,6 +20,42 @@ public class Heap {
         fixHeapAbove(size++);
     }
 
+    public int peek() {
+        if (isEmpty()) {
+            throw new IndexOutOfBoundsException("Heap is empty");
+        }
+
+        return heap[0];
+    }
+
+    // taking the index means we can do a binary search
+    public int delete(int index) {
+        if (isEmpty()) {
+            throw new IndexOutOfBoundsException("Heap is empty");
+        }
+
+        // store parent and deleted value
+        int parent = getParent(index);
+        int deletedValue = heap[index];
+
+        // right most value replaces whatever was in the index
+        heap[index] = heap[size - 1];
+
+        // tree is still complete but now we have to fix the heap
+        // if dealing with the root or this is less than the parent
+        // ie, we don't have to worry about anything above
+        if (index == 0 || heap[index] < heap[parent]) {
+            fixHeapBelow(index, size - 1);
+        } else {
+            // we do have to worry about elements above
+            fixHeapAbove(index);
+        }
+
+        size--;
+
+        return deletedValue;
+    }
+
     // for inserts, we have to fix the heap by travelling upwards
     private void fixHeapAbove(int index) {
         // store the value we've just inserted
@@ -35,11 +71,68 @@ public class Heap {
         heap[index] = newValue;
     }
 
+    // we'll need lastHeapIndex for when we do the heap sort
+    private void fixHeapBelow(int index, int lastHeapIndex) {
+        int childToSwap;
+
+        while (index <= lastHeapIndex) {
+            // get two children where value we deleted was
+            int leftChild = getChild(index, true);
+            int rightChild = getChild(index, false);
+
+            // NOTE: heap can only have both or left at this point
+            // if there is a left child
+            if (leftChild <= lastHeapIndex) {
+                // if there is NOT a right child
+                if (rightChild > lastHeapIndex) {
+                    // swap with left child by default
+                    childToSwap = leftChild;
+                } else {
+                    // swap with the larger of the two
+                    childToSwap = heap[leftChild] > heap[rightChild] ? leftChild : rightChild;
+                }
+
+                // if value at index is less than the child we'd swap with
+                if (heap[index] < heap[childToSwap]) {
+                    // we have to swap them
+                    int tmp = heap[index];
+                    heap[index] = heap[childToSwap];
+                    heap[childToSwap] = tmp;
+                } else {
+                    // we don't need to do anything; heap satisfies heap property
+                    break;
+                }
+
+                // update index; compare replacement value with its new children
+                index = childToSwap;
+            } else {
+                // there are no children, so there's nothing to do
+                break;
+            }
+        }
+    }
+
     public boolean isFull() {
         return size == heap.length;
     }
 
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
     public int getParent(int index) {
         return (index - 1) / 2;
+    }
+
+    public int getChild(int index, boolean left) {
+        return 2 * index + (left ? 1 : 2); 
+    }
+
+    public void printHeap() {
+        for (int i = 0; i < size; i++) {
+            System.out.print(heap[i]);
+            System.out.print(", ");
+        }
+        System.out.println();
     }
 }
