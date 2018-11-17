3328bf0bced9d041e0d14f489ab0ec70cac24fba
Connor <cafitzp1@asu.edu>
2018-11-16 23:20:35 -0700

    check if two arrays are rotations of each other

--- src/main/java/my/java/app/App.java
+++ src/main/java/my/java/app/App.java
@@ -2,8 +2,45 @@ package my.java.app;
 
 public class App {
     public static void main(String[] args) {
+        // NOTE: The following input values will be used for testing your solution.
+        int[] array1 = {1, 2, 3, 4, 5, 6, 7};
+        int[] array2a = {4, 5, 6, 7, 8, 1, 2, 3};
+        System.out.println(isRotation(array1, array2a));
+        // isRotation(array1, array2a) should return false.
+        int[] array2b = {4, 5, 6, 7, 1, 2, 3};
+        System.out.println(isRotation(array1, array2b));
+        // isRotation(array1, array2b) should return true.
+        int[] array2c = {4, 5, 6, 9, 1, 2, 3};
+        System.out.println(isRotation(array1, array2c));
+        // isRotation(array1, array2c) should return false.
+        int[] array2d = {4, 6, 5, 7, 1, 2, 3};
+        System.out.println(isRotation(array1, array2d));
+        // isRotation(array1, array2d) should return false.
+        int[] array2e = {4, 5, 6, 7, 0, 2, 3};
+        System.out.println(isRotation(array1, array2e));
+        // isRotation(array1, array2e) should return false.
+        int[] array2f = {1, 2, 3, 4, 5, 6, 7};
+        System.out.println(isRotation(array1, array2f));
+        // isRotation(array1, array2f) should return true.
+    }
 
-        System.out.println("Hi there");
+    // Implement your solution below.
+    public static Boolean isRotation(int[] array1, int[] array2) {
 
+        int matches = 0;
+
+        for (int i = 0, j = 0, k = 0; i < array1.length && k < array2.length; j++) {
+            j %= array1.length;
+            if (array1[i] != array2[j]) {
+                matches = 0;
+                k++;
+            } else {
+                k = 0;
+                i++;
+                matches++;
+            }
+        }
+
+        return matches == array1.length ? true : false;
     }
 }
