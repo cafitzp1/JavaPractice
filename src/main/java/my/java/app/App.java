cfb51f9bf17db85623ddc7d34d0a3ab055acfdcd
Connor <cafitzp1@asu.edu>
2018-11-17 21:19:01 -0700

    my solution using recursion

--- src/main/java/my/java/app/App.java
+++ src/main/java/my/java/app/App.java
@@ -1,9 +1,29 @@
 package my.java.app;
 
+import java.util.HashSet;
+
 public class App {
     public static void main(String[] args) {
+        // NOTE: The following input values will be used for testing your solution.
+        System.out.println((nonRepeating("abcab"))); // should return 'c'
+        System.out.println((nonRepeating("abab"))); // should return null
+        System.out.println((nonRepeating("aabbbc"))); // should return 'c'
+        System.out.println((nonRepeating("aabbdbc"))); // should return 'd'
+    }
 
-        System.out.println("Hi there");
+    // Implement your solution below.
+    public static Character nonRepeating(String s) {
 
+        if ( s == null || s.length()==0)
+            return null;
+
+        Character firstChar = s.charAt(0);
+        for (int i = 1; i < s.length(); i++) {
+            if (firstChar == s.charAt(i)) {
+                return nonRepeating(s.replace(firstChar.toString(), ""));
+            }
+        }
+
+        return firstChar;
     }
 }
