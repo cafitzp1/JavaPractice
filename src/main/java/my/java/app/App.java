1af2e536b1dda9e2c7183ec90a428e9929f4ecf0
Connor <cafitzp1@asu.edu>
2018-11-19 13:19:06 -0700

    my solution using dynamic programming

--- src/main/java/my/java/app/App.java
+++ src/main/java/my/java/app/App.java
@@ -3,7 +3,34 @@ package my.java.app;
 public class App {
     public static void main(String[] args) {
 
-        System.out.println("Hi there");
+        System.out.println(longestCommonSubstring("abcdaf", "bcdaddd"));
+    }
 
+    private static String longestCommonSubstring(String s1, String s2) {
+
+        Integer[][] matrix = new Integer[s1.length() + 1][s2.length() + 2];
+        int longest = 0;
+        int[] longestXY = new int[] { 0, 0 };
+
+        for (int i = 0; i < s1.length(); i++) {
+            for (int j = 0; j < s2.length(); j++) {
+                if (s1.charAt(i) == s2.charAt(j)) {
+                    int len = matrix[i][j] == null ? 1 : matrix[i][j] + 1;
+                    matrix[i + 1][j + 1] = len;
+                    if (len > longest) {
+                        longest = len;
+                        longestXY = new int[] { i + 1, j + 1 };
+                    }
+                }
+            }
+        }
+
+        String sr = "";
+
+        for (int i = 0; i < longest; i++) {
+            sr = s1.charAt(longestXY[0] - i - 1) + sr;
+        }
+
+        return sr;
     }
 }
