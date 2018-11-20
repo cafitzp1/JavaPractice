9a09eed2b3191aa61f7a6d539ff51305496a07bd
Connor <cafitzp1@asu.edu>
2018-11-19 20:50:48 -0700

    preferred solution from cs dojo

--- src/main/java/my/java/app/App.java
+++ src/main/java/my/java/app/App.java
@@ -3,7 +3,23 @@ package my.java.app;
 public class App {
     public static void main(String[] args) {
 
-        System.out.println("Hi there");
+        System.out.println(maxSumSubarray(new int[] { -2, 3, 2, 1, -1, 4 }));
 
+    }
+
+    private static int maxSumSubarray(int[] arr) {
+        int max = 0, curMax = 0;
+
+        for (int i = 0, cur = 0; i < arr.length; i++) {
+            cur = Math.max(arr[i], arr[i] + curMax);
+            if (cur > curMax) {
+                curMax = cur;
+                if (curMax > max) {
+                    max = curMax;
+                }
+            }
+        }
+
+        return max;
     }
 }
