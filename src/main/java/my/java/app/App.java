e26eced1bff4a73498f1a582f9ecd6ced0d34db4
Connor <cafitzp1@asu.edu>
2018-11-19 23:41:39 -0700

    removes duplicates from a sorted array in place in the sense that the first elements are overwritten (array is not resized)

--- src/main/java/my/java/app/App.java
+++ src/main/java/my/java/app/App.java
@@ -3,7 +3,24 @@ package my.java.app;
 public class App {
     public static void main(String[] args) {
 
-        System.out.println("Hi there");
+        System.out.println(removeDuplicates(new int[] { 1, 1, 1, 1, 1, 1, 1, 2, 1, 1, 2 }));
 
+    }
+
+    public static int removeDuplicates(int[] nums) {
+        
+        if (nums.length == 0) 
+            return 0;
+        
+        int i = 0;
+        
+        for (int j = 1; j < nums.length; j++) {
+            if (nums[j] != nums[i]) {
+                i++;
+                nums[i] = nums[j];
+            }
+        }
+        
+        return i + 1;
     }
 }
