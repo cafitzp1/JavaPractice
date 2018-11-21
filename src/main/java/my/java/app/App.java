5da2ee679f01785791001590d3c4fcdda9e0e951
Connor <cafitzp1@asu.edu>
2018-11-21 01:29:20 -0700

    hashing function using total of ASCII characters modded by 2069

--- src/main/java/my/java/app/App.java
+++ src/main/java/my/java/app/App.java
@@ -3,7 +3,20 @@ package my.java.app;
 public class App {
     public static void main(String[] args) {
 
-        System.out.println("Hi there");
+        System.out.println(hash("hello")%10);
+        System.out.println(hash("good-bye")%10);
+        
+    }
 
+    private static int hash(String key) {
+
+        char[] keyChars = key.toCharArray();
+        int hashedKey = 0;
+
+        for (char c : keyChars)
+            hashedKey += c;
+        hashedKey %= 2069;
+
+        return hashedKey;
     }
 }
